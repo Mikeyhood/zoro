9275212b75df57a83277a2ca1e5f655eb8d2e074
Michael <michael.hood99@gmail.com>
2016-09-07 21:36:03 -0400

    Minor Dashboard Changes
    working on popup for styling POP experience

--- Sello/index.js
+++ Sello/index.js
@@ -1,18 +1,27 @@
-let express=require("express");
-let app=express();
-let port=3000;
+let express=require("express"); //imports Express so you can use node as a web server
+let app=express(); //opens up a new web server
+let port=3000; // sets the port number to use, i.e. http://locahost:port/
 
-app.use(express.static("public"))
+app.use(express.static("public")) //set the folder where all the assets are
 
+//default route, ie. http://locahost:3000/
 app.get("/",function (req,res){
     
 res.sendFile("/public/index.html", {root:__dirname})    
     
 })
 
-app.get("/index2",function (req,res){
+// second route, ie. http://locahost:3000/onboarding
+app.get("/onboarding",function (req,res){
     
-res.sendFile("/public/index2.html", {root:__dirname})    
+res.sendFile("/public/onboarding.html", {root:__dirname})    
     
 })
+app.get("/home",function (req,res){
+    
+res.sendFile("/public/home.html", {root:__dirname})    
+    
+})
+
+//start the actual web server
 app.listen(port)
